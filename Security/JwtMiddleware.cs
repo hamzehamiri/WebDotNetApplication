ff35c494810143195e5f5cb3556b2ec0ad2cd737
HamzehAmiri <solidworkerh@gmail.com>
2023-12-11 17:14:17 +0330

    first commit

---
+++ Security/JwtMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication1.Security
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                // Validate the token
+                // ...
+            }
+
+            await _next(context);
+        }
+    }
+}
